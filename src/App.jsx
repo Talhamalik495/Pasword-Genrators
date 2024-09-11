29e94092ff038e9c15eb63969bcb3fab5f290599
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-09-12 00:10:56 +0500

    70% work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useCallback } from 'react';
-import './App.css';
+import React, { useState, useCallback, useEffect } from 'react';
 
 function App() {
+  // console.clear()
   let [length,setLength]= useState(10)
   let [number,setNumber]= useState(false)
   let [character,setCharacter]= useState(false)
@@ -13,20 +13,44 @@ function App() {
     let specialCharacter="!@#$%^&*-_+=[]{}~`"
     if (number) str+=num
     if (character) str+=specialCharacter
-    if (character) str+=specialCharacter
-    for (let i = 0; i < str.length; i++) {
-      const char= Math.floor(Math.random()*str.length)
-  
-      
+    for (let i = 1; i <= length; i++) {
+      console.log(str);
+      const char1=Math.random()*str.length
+      console.log(char1);
+      const char= Math.floor(Math.random()*str.length+1)
+      console.log(char);
+      newPasword+=str.charAt(char) 
     }
-    },[])
+    setPassword(newPasword)
+    },[length,number,character,setPassword])
+    useEffect(()=>{
+      passworGenrat()
+    },[length,number,character,setPassword])
   return (
     <>
-  <h1 className='text-white text-center font-bold font-mono text-4xl'>Password Genrators</h1>
 
-  <div className='w-6/12 h-80 bg-orange-800 flex justify-center align-middle flex-col'>
-<input className='w-96' type="text" />
-<button className='w-40 bg-blue-400 text-white'>Copy</button>
+  <br />
+  <br />
+  
+  <div className='text-white max-w-xl mx-auto bg-gray-700 flex px-8 py-10 flex-col rounded-lg shadow-md gap-2' >
+  <h1 className='text-white text-center font-bold font-mono text-4xl'>Password Genrators</h1>
+  <br />
+  <div className='flex justify-center align-middle gap-2'>
+<input className='w-full outline-none text-black  bg-white rounded-lg h-10' type="text "placeholder='password' readOnly value={Password}/>
+<button className='w-40 h-10 rounded-md  bg-blue-400 text-white'>Copy</button>
+</div>
+<div className='flex gap-4'>
+<label htmlFor="character">character</label>
+<input defaultChecked={character}  type="checkbox" id='character'  value={character}  onChange={(e)=>setCharacter((prev)=> !prev)}/>
+</div>
+<div className='flex gap-x-7'>
+<label htmlFor="number">number</label>
+<input type="checkbox" defaultChecked={number} id='number' value={number} onChange={(e)=>setNumber((prev)=> !prev)} />
+</div>
+<div className='flex gap-2'>
+  <label>length:{length}</label>
+  <input type="range" value={length} min={6} max={100} onChange={(e)=> setLength(e.target.value)}/>
+</div>
   </div>
     </>
   )
