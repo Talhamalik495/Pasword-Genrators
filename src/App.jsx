d035d1d662b920d738caeb8c3dae9b17947d9a62
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2025-01-11 20:47:05 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,91 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect } from "react";
 
 function App() {
   // console.clear()
-  let [length,setLength]= useState(10)
-  let [number,setNumber]= useState(false)
-  let [character,setCharacter]= useState(false)
-  let [Password,setPassword]= useState("")
-  let passworGenrat=useCallback(()=>{
-    let newPasword=""
-    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    let num=12345678910
-    let specialCharacter="!@#$%^&*-_+=[]{}~`"
-    if (number) str+=num
-    if (character) str+=specialCharacter
+  let [length, setLength] = useState(10);
+  let [number, setNumber] = useState(false);
+  let [character, setCharacter] = useState(false);
+  let [Password, setPassword] = useState("");
+  let passworGenrat = useCallback(() => {
+    let newPasword = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let num = 12345678910;
+    let specialCharacter = "!@#$%^&*-_+=[]{}~`";
+    if (number) str += num;
+    if (character) str += specialCharacter;
     for (let i = 1; i <= length; i++) {
       console.log(i);
-      const char1=Math.random()*str.length
+      const char1 = Math.random() * str.length;
       console.log(char1);
-      const char= Math.floor(Math.random()*str.length+1)
+      const char = Math.floor(Math.random() * str.length + 1);
       console.log(char);
-      newPasword+=str.charAt(char) 
+      newPasword += str.charAt(char);
     }
-    setPassword(newPasword)
-    },[length,number,character,setPassword])
-    useEffect(()=>{
-      passworGenrat()
-    },[length,number,character,setPassword])
+    setPassword(newPasword);
+  }, [length, number, character, setPassword]);
+  useEffect(() => {
+    passworGenrat();
+  }, [length, number, character, setPassword]);
   return (
     <>
+      <br />
+      <br />
+      <div className="flex flex-col gap-5 justify-center items-center">
+        <div className="flex pb-10">
+          <h1 className="text-black text-center font-bold text-4xl">
+            Password Genrators
+          </h1>
+        </div>
+        <div className="text-black max-w-xl mx-auto bg-white shadow-lg shadow-black flex px-8 py-10 flex-col rounded-lg  gap-5">
+          <div className="flex justify-center align-middle gap-2">
+            <input
+              className="w-full  text-black  bg-white  h-10 px-2 border-[1px] border-black rounded-lg"
+              type="text "
+              placeholder="password"
+              readOnly
+              value={Password}
+            />
+            <button className="w-40 h-10 rounded-md  bg-blue-400 text-black">
+              Copy
+            </button>
+          </div>
+          <div className="flex gap-5">
+            <div className="flex gap-1">
+              <label>length:{length}</label>
+              <input
+                type="range"
+                value={length}
+                min={6}
+                max={100}
+                onChange={(e) => setLength(e.target.value)}
+              />
+            </div>
 
-  <br />
-  <br />
-  
-  <div className='text-white max-w-xl mx-auto bg-gray-700 flex px-8 py-10 flex-col rounded-lg shadow-md gap-5' >
-  <h1 className='text-white text-center font-bold font-serif text-4xl'>Password Genrators</h1>
-  <div className='flex justify-center align-middle gap-2'>
-<input className='w-full outline-none text-black  bg-white rounded-lg h-10 px-2' type="text "placeholder='password' readOnly value={Password}/>
-<button className='w-40 h-10 rounded-md  bg-blue-400 text-white'>Copy</button>
-</div>
-<div className='flex gap-5'>
-<div className='flex gap-1'>
-  <label>length:{length}</label>
-  <input type="range" value={length} min={6} max={100} onChange={(e)=> setLength(e.target.value)}/>
-</div>
-
-<div className='flex gap-1'>
-<label htmlFor="number">Number</label>
-<input type="checkbox" defaultChecked={number} id='number' value={number} onChange={(e)=>setNumber((prev)=> !prev)} />
-</div>
-<div className='flex gap-1'>
-<label htmlFor="character">Character</label>
-<input defaultChecked={character}  type="checkbox" id='character'  value={character}  onChange={(e)=>setCharacter((prev)=> !prev)}/>
-</div>
-</div>
-  </div>
+            <div className="flex gap-1">
+              <label htmlFor="number">Number</label>
+              <input
+                type="checkbox"
+                defaultChecked={number}
+                id="number"
+                value={number}
+                onChange={(e) => setNumber((prev) => !prev)}
+              />
+            </div>
+            <div className="flex gap-1">
+              <label htmlFor="character">Character</label>
+              <input
+                defaultChecked={character}
+                type="checkbox"
+                id="character"
+                value={character}
+                onChange={(e) => setCharacter((prev) => !prev)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
